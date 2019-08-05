f6065962ef2f6d043f39a345c016d45cef70b1e5
ysw <ysw@yswdeMacBook-Pro.local>
2019-08-05 11:31:16 +0800

    商品推送选择

---
+++ Suo/GAGoodsPushSelectView.h
@@ -0,0 +1,19 @@
+//
+//  GAGoodsPushSelectView.h
+//  Suo
+//
+//  Created by ysw on 2019/8/5.
+//  Copyright © 2019 怪兽 🐙. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface GAGoodsPushSelectView : UIView
+
+- (void)show;
+- (void)dismiss;
+@end
+
+NS_ASSUME_NONNULL_END
